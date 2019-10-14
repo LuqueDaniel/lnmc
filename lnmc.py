57a0808e229d04744bcbd0517a20389fb9b1f0fb
Daniel Luque <danielluque14@gmail.com>
2019-10-15 00:18:07 +0200

    First commit

---
+++ lnmc.py
@@ -0,0 +1,88 @@
+"""lnmc - Allows to create symbolic link in batches from a YAML file."""
+
+import pathlib
+
+import click as cli
+import yaml
+
+
+def yaml_read(yaml_file: str) -> dict:
+    """Read the YAML file and return a dictionary."""
+    with open(yaml_file, "r") as stream:
+        return yaml.safe_load(stream.read())
+
+
+def symlink_create(src: pathlib.Path, dst: pathlib.Path, rewrite: bool,
+                   verbose: bool) -> None:
+    """Create the symbolic link.
+
+    Check that there aren't other files or directories with the same name in
+    the destination directory. If the symbolic link already exists it will be
+    ignored, unless the `rewrite` argument is True.
+
+    Args:
+        src (pathlib.Path): symbolic link origin path.
+        dst (pathlib.Path): destination directory where the symbolic link will
+                            be created.
+        rewrite (bool): overwrite symbolic links if exist.
+        verbose (bool): show log messages.
+
+    Returns: None
+    """
+    if dst.exists() and rewrite and dst.is_symlink():
+        if verbose:
+            cli.secho(f"Symbolic link exists: {dst}", fg='cyan')
+            cli.secho(f"\tUnlinking: {dst}", fg='yellow')
+        dst.unlink()
+    elif dst.exists() and not dst.is_symlink():
+        cli.secho(f"Can't create symlink. The file or directory: "
+                  f"{dst} already exists.", bold=True, fg='red')
+        return
+    elif dst.exists() and not rewrite:
+        return
+
+    if verbose:
+        cli.secho(f"Creating symlink: {dst}", fg='green', bold=True)
+
+    dst.resolve().symlink_to(src.resolve())
+
+
+@cli.command()
+@cli.argument('yaml_file', type=cli.Path(exists=True))
+@cli.argument('src', type=cli.Path(exists=True))
+@cli.argument('dst', type=cli.Path(exists=True))
+@cli.option('--rewrite/--no-rewrite', default=False,
+            help="Overwrite thesymbolic links if exist.")
+@cli.option('--verbose', is_flag=True, help="Enables verbose mode.")
+@cli.version_option(version="1.0.0", prog_name="lnmc")
+def lnmc(yaml_file: str, src: str, dst: str, rewrite: bool,
+         verbose: bool) -> None:
+    """lnmc allows to create symbolic links in batches.
+
+    The files, directories and sub-directories that are going to be targeted to
+    create the symbolic links are specified in a yaml file.
+
+    $ lnmc directories.yaml ~/destination_directory source_directory/
+    """
+    if verbose:
+        cli.echo(f"Reading {cli.style(yaml_file, fg='green')} file.")
+    dirs = yaml_read(yaml_file)
+
+    for directory in dirs:
+        src_path = pathlib.Path(src).joinpath(directory)
+
+        if dirs[directory] is None:
+            subdirs = src_path.iterdir()
+        else:
+            subdirs = dirs[directory]
+
+        for item in subdirs:
+            if hasattr(item, 'name'):
+                item = item.name
+
+            symlink_create(src_path / item, pathlib.Path(dst).joinpath(item),
+                           rewrite, verbose)
+
+
+if __name__ == "__main__":
+    lnmc()
---
+++ tests/lnmc_test.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import sys
+import shutil
+
+import pytest
+from click.testing import CliRunner
+
+import lnmc
+
+sys.path.insert(0, '..')
+
+YAML_TEST_FILE = 'tests/test.yaml'
+SRC = Path('tests/src')
+DST = Path('tests/dst')
+
+
+@pytest.fixture(scope='module')
+def setup(request):
+    """Create src and dst test hierarchy and cleanup after test finalize."""
+    SRC.mkdir()
+    for subdir in range(3):
+        subdir = Path(f'{SRC}/subdir {subdir}')
+        subdir.mkdir()
+        for file_ in range(4):
+            Path(f'{subdir}/file {file_}.txt').touch()
+    DST.mkdir()
+
+    request.addfinalizer(cleanup)
+
+
+def cleanup():
+    shutil.rmtree(SRC)
+    shutil.rmtree(DST)
+
+
+def test_yaml_read():
+    result = lnmc.yaml_read(YAML_TEST_FILE)
+    assert result is not None
+    assert isinstance(result, dict)
+
+
+@pytest.mark.parametrize('rewrite', [False, True, False])
+def test_symlink_create(setup, rewrite):
+    """Try to create symbolic links with different values ​​in the 'rewrite'
+    argument."""
+    src = Path(f"{SRC}/subdir 0/file 3.txt")
+    dst = Path(f"{DST}/file 3.txt")
+    lnmc.symlink_create(src, dst, rewrite=rewrite, verbose=True)
+
+
+def test_symlink_create_check(setup):
+    """Check if the symbolic link has been created."""
+    dst = Path(f"{DST}/file 3.txt")
+    lnmc.symlink_create(Path(f"{SRC}/subdir 1/file 3.txt"),
+                        dst, rewrite=False, verbose=True)
+    assert dst.exists()
+    assert dst.is_symlink()
+
+
+def test_symlink_create_file_exists(setup):
+    """Test symlink_create when file or directory already exists."""
+    dst = DST.joinpath('file 2.txt')
+    dst.touch()
+    lnmc.symlink_create(Path(f"{SRC}/subdir 1/file 2.txt"),
+                        dst, rewrite=False, verbose=True)
+    dst.unlink()
+
+
+def test_lnmc(setup):
+    """Test the full command yaml file included."""
+    runner = CliRunner()
+    runner.invoke(lnmc.lnmc, [YAML_TEST_FILE, str(SRC), str(DST), '--rewrite'])
+
+    yaml_file = lnmc.yaml_read(YAML_TEST_FILE)
+
+    for directory in yaml_file:
+        for item in yaml_file[directory]:
+            assert DST.joinpath(item).exists()
+            assert DST.joinpath(item).is_symlink()
