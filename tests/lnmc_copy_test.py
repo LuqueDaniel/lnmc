4e7d404d2d13c5641eb2b42fe80610d3d82398da
Daniel Luque <danielluque14@gmail.com>
2023-01-15 18:27:54 +0100

    Refactor _copy_item

--- tests/lnmc_copy_test.py
+++ tests/lnmc_copy_test.py
@@ -9,18 +9,16 @@ from .helpers import DST, SRC, files_setup, filesystem_actions
     "rewrite", [False, True, False], ids=["not exists", "overwrite", "Can't copy"]
 )
 def test_copy_item(files_setup, filesystem_actions, rewrite, capsys):
-    """Try to copy with different values ​​in 'rewrite' argument."""
+    """Try to copy with different values in 'rewrite' argument."""
     filesystem_actions.rewrite = rewrite
     directories = {"dir 0": ["dir_copy"]}
     filesystem_actions.copy(directories)
 
     dst = PurePath(DST / "dir_copy")
     captured = capsys.readouterr()
-    if captured.out.startswith(f"Copying: {dst}"):
+    if captured.out.startswith(f"Copying {dst}"):
         assert True
-    elif captured.out.startswith(
-        f"Can't copy. The file or directory: {dst} already exists."
-    ):
+    elif captured.out.startswith(f"A file or directory already exists: {dst}"):
         assert True
     else:
         assert False
